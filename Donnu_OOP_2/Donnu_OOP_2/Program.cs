b5060418a0f2f2813436f1a2aacb38f4a47e5d35
MaroderKo <MaroderKo@users.noreply.github.com>
2020-09-23 16:28:01 +0300

    Second

---
+++ Donnu_OOP_2/Donnu_OOP_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donnu_OOP_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Введіть змінну типу інт:");
+            string str = Console.ReadLine();
+            try
+            {
+                int rez = Convert.ToInt32(str);
+                Console.WriteLine(rez);
+            }
+            catch
+            {
+                Console.WriteLine("Помилка під час вводу змiнної Int!");
+            }
+            
+            Console.WriteLine("Введіть змінну типу Double:");
+            str = Console.ReadLine();
+            try
+            {
+                double rez = Convert.ToDouble(str);
+                Console.WriteLine(rez);
+            }
+            catch
+            {
+                Console.WriteLine("Помилка під час вводу змiнної Double!");
+            }
+
+            Console.WriteLine("Введіть змінну типу Long:");
+            str = Console.ReadLine();
+            try
+            {
+                long rez = Convert.ToInt64(str);
+                Console.WriteLine(rez);
+            }
+            catch
+            {
+                Console.WriteLine("Помилка під час вводу змiнної Long!");
+            }
+
+
+
+        }
+    }
+}
---
+++ Donnu_OOP_3/Donnu_OOP_3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donnu_OOP_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean Flag = false;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            while (!Flag)
+            {
+                Console.WriteLine("Введіть змінну типу інт:");
+                string str = Console.ReadLine();
+                try
+                {
+                    int rez = Convert.ToInt32(str);
+                    Console.WriteLine(rez);
+                    Flag = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Помилка під час вводу змiнної Int!");
+                }
+            }
+            Flag = false;
+            while (!Flag)
+            {
+                Console.WriteLine("Введіть змінну типу Double:");
+                string str = Console.ReadLine();
+                try
+                {
+                    double rez = Convert.ToDouble(str);
+                    Console.WriteLine(rez);
+                    Flag = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Помилка під час вводу змiнної Double!");
+                }
+            }
+            Flag = false;
+            while (!Flag)
+            {
+                Console.WriteLine("Введіть змінну типу Long:");
+                string str = Console.ReadLine();
+                try
+                {
+                    long rez = Convert.ToInt64(str);
+                    Console.WriteLine(rez);
+                }
+                catch
+                {
+                    Console.WriteLine("Помилка під час вводу змiнної Long!");
+                }
+            }
+
+
+
+        }
+    }
+}
