fa8a890928a9267c7a77c770282a5e4275f947d4
MaroderKo <MaroderKo@users.noreply.github.com>
2020-10-07 13:21:31 +0300

    4-th

---
+++ donnu_oop_4-5/donnu_oop_4_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace donnu_oop_4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
